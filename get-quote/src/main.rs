cbbb2a81c7404562d91cdec39e26762953edfa43
heytdep <tommaso@xycloo.com>
2025-07-11 14:29:04 +0200

    test get quote

---
+++ get-quote/src/main.rs
@@ -0,0 +1,14 @@
+pub(crate) async fn get_quote_from_dstack(report_data: &str) -> anyhow::Result<String> {
+    Ok(
+        reqwest::get(format!("http://0.0.0.0:3030/quote/{}", report_data))
+            .await?
+            .text()
+            .await?,
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let quote = get_quote_from_dstack("testdata").await;
+    println!("{:?}", quote);
+}
--- measurements/src/main.rs
+++ measurements/src/main.rs
@@ -4,31 +4,9 @@ use std::process;
 
 use sha2::Digest;
 
-fn aggregate_pods(pods: Vec<[u8;48]>) -> Vec<u8> {
+fn aggregate_pods(pods: Vec<[u8; 48]>) -> Vec<u8> {
     pods.concat().to_vec()
 }
-
-fn powerset<T: Clone>(set: &[T]) -> Vec<Vec<T>> {
-    // nb: we probably don't want to powerset here just have clear guidelines
-    // on how to deploy in a correct sequence so we need to check less cases, also
-    // because powerset is actually wrong here it should be permutations and for each
-    // "lenght".
-    let n = set.len();
-    let mut result = Vec::new();
-
-    for mask in 1..(1 << n) {
-        let mut subset = Vec::new();
-        for i in 0..n {
-            if mask & (1 << i) != 0 {
-                subset.push(set[i].clone());
-            }
-        }
-        result.push(subset);
-    }
-
-    result
-}
-
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -42,16 +20,13 @@ fn main() {
 
     for path in &args {
         if let Ok(pod_config) = fs::read(path) {
-            let pod_hash: [u8;48] = sha2::Sha384::digest(&pod_config).try_into().unwrap();
+            let pod_hash: [u8; 48] = sha2::Sha384::digest(&pod_config).try_into().unwrap();
             loaded_pods.push(pod_hash);
         } else {
             println!("warn: file doesn't exist")
         }
     }
 
-    for subset in powerset(&loaded_pods) {
-        let aggregated = aggregate_pods(subset.clone());
-        
-        println!("Measurement: {}", hex::encode(aggregated));
-    }
+    let aggregated = aggregate_pods(loaded_pods);
+    println!("Measurement: {}", hex::encode(aggregated));
 }
