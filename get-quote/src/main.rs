aa10bac571c420e9c2b946bc8172911eaa0ab803
heytdep <tommaso@xycloo.com>
2025-07-11 15:15:40 +0200

    actually hex encode

--- get-quote/src/main.rs
+++ get-quote/src/main.rs
@@ -1,6 +1,6 @@
 pub(crate) async fn get_quote_from_dstack(report_data: &str) -> anyhow::Result<String> {
     Ok(
-        reqwest::get(format!("http://0.0.0.0:3030/quote/{}", report_data))
+        reqwest::get(format!("http://0.0.0.0:3030/quote/{}", hex::encode(report_data)))
             .await?
             .text()
             .await?,
